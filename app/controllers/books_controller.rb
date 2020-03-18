fdf5237ea18e82887d1303eff279032f232ef2b4
satotaichiro <tapoya09@gmail.com>
2020-03-18 21:35:37 +0900

    add[5-9]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,44 @@
 class BooksController < ApplicationController
 before_action :authenticate_user!
   def index
-  	@books = Post.all
-  	@book = Post.new
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def create
-    @book = Post.new(book_params)
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
   if @book.save
     flash[:notice] = 'Book was successfully created.'
     redirect_to book_path(@book.id)
   else
-    @books = Post.all
+    @books = Book.all
     render action: :index
   end
 end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
   end
 
   def top
   end
+
+def update
+    book = Book.find(params[:id])
+  if book.update(book_params)
+    redirect_to book_path(book), notice: 'Book was successfully created.'
+  else
+    render action: :index
+  end
+end
 
   private
    
